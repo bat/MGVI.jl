cc332046d09fe80c595e2fabc8bf07fd4a95de93
Oliver Schulz <oschulz@mpp.mpg.de>
2020-10-29 17:42:53 +0100

    Add dependencies

--- src/MGVInference.jl
+++ src/MGVInference.jl
@@ -5,9 +5,41 @@ __precompile__(true)
 """
     MGVInference
 
-Template for Julia packages.
+An implementation of the Metric Gaussian Variational Inference algorithm.
 """
 module MGVInference
+
+using Distributed
+using LinearAlgebra
+using Random
+using SparseArrays
+using Statistics
+
+using ArgCheck
+using ArraysOfArrays
+using BlockDiagonals
+using ChainRules
+using ChainRulesCore
+using DiffResults
+using Distributions
+using DistributionsAD
+using FFTW
+using FillArrays
+using FiniteDiff
+using ForwardDiff
+using NLSolversBase
+using Optim
+using Parameters
+using PositiveFactorizations
+using ProgressMeter
+using Random123
+using SparseDiffTools
+using SparsityDetection
+using Statistics
+using StatsBase
+using ValueShapes
+using Zygote
+using ZygoteRules
 
 include("mgvi.jl")
 
