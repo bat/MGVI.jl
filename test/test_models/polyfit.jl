c3da375a2334707050924a1e0c48ed3ade5838c6
Victor Ananyev <vindex10@gmail.com>
2020-11-11 00:43:45 +0100

    test_models unified approach. add fft_gp

--- test/test_models/polyfit.jl
+++ test/test_models/polyfit.jl
@@ -8,13 +8,13 @@ _x1_grid = [Float64(i)/10 for i in 1:25]
 _x2_grid = [Float64(i)/10 + 0.1 for i in 1:15]
 _common_grid = sort(vcat(_x1_grid, _x2_grid))
 
-function _line(x_grid, p)
+function _mean(x_grid, p)
     p[1]*10 .+ p[2]*40 .* x_grid .+ p[3]*600 .* x_grid.^2 .+ p[4]*80 .* x_grid.^3
 end
 
-function full_model(p)
-    dist1 = Product(Normal.(_line(_x1_grid, p), p[5]^2*60))
-    dist2 = Product(Normal.(_line(_x2_grid, p), p[5]^2*60))
+function model(p)
+    dist1 = Product(Normal.(_mean(_x1_grid, p), p[5]^2*60))
+    dist2 = Product(Normal.(_mean(_x2_grid, p), p[5]^2*60))
     NamedTupleDist(a=dist1,
                    b=dist2)
 end
@@ -34,4 +34,4 @@ starting_point = [
  -0.6
 ]
 
-export full_model, true_params, starting_point
+export model, true_params, starting_point
