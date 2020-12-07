1936f17f763e1a60c1cf7e21b0edc4d5446f7a8f
Victor Ananyev <vindex10@gmail.com>
2020-12-07 22:15:29 +0100

    Refactor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,30 +1,12 @@
 # This file is a part of MGVInference.jl, licensed under the MIT License (MIT).
 
-using Random
-using Distributions
-using DistributionsAD
-using HypothesisTests
-using LinearAlgebra
-using SparseArrays
-using ValueShapes
-import Zygote
-
-using MGVInference
-
-include("test_models/model_polyfit.jl")
-import .ModelPolyfit
-
-include("test_models/model_fft_gp.jl")
-import .ModelFFTGP
-
 import Test
 
 Test.@testset "Package MGVInference" begin
 
-include("utils.jl")
 include("test_mgvi.jl")
 include("test_jacobians.jl")
-include("test_information.jl")
+include("information/test_information.jl")
 include("test_samplers.jl")
 
 end # testset
