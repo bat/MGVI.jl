7583e33e94066acb0c65e3981b9f7206f53501cd
Victor Ananyev <vindex10@gmail.com>
2020-12-07 14:37:49 +0100

    reformulate test for samplers

--- test/test_samplers.jl
+++ test/test_samplers.jl
@@ -14,14 +14,14 @@ Test.@testset "test_cmp_residual_samplers" begin
     full_rs = FullResidualSampler(fisher, jac)
     full_samples_1 = rand(Random.GLOBAL_RNG, full_rs, num_of_test_samples)
     full_samples_2 = rand(Random.GLOBAL_RNG, full_rs, num_of_test_samples)
-    p0 = BartlettTest(full_samples_1', full_samples_2') |> pvalue
+    B0 = BartlettTest(full_samples_1', full_samples_2')
 
-    Test.@test p0 > 5E-2
+    Test.@test pvalue(B0) > 5E-2
 
     implicit_rs = ImplicitResidualSampler(fisher, jac)
     implicit_samples = rand(Random.GLOBAL_RNG, implicit_rs, num_of_test_samples)
-    p = BartlettTest(full_samples_1', implicit_samples') |> pvalue
+    B1 = BartlettTest(full_samples_1', implicit_samples')
 
-    Test.@test p > p0/10
+    Test.@test B1.L′ > B0.L′ 
 
 end
