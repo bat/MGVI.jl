68f791b26284b0d954c5706d60fec8692fc180df
Oliver Schulz <oschulz@mpp.mpg.de>
2022-03-09 17:31:05 +0100

    Improve Jacobian implementations

--- src/jacobian_maps.jl
+++ src/jacobian_maps.jl
@@ -5,7 +5,7 @@
 
 Abstract type of the jacobian calculators.
 
-Represents the jacobian of the function. The instance can be called at the point `θ`,
+Represents the jacobian of the function. The instance can be called at the point `x`,
 a LinearMap representing the jacobian is returned.
 """
 abstract type AbstractJacobianFunc <: Function end
@@ -18,31 +18,90 @@ function _dual_along(f::F, x::AbstractVector{T1}, δ::AbstractVector{T2}) where 
     f(ForwardDiff.Dual{T_Dual}.(x, δ))
 end
 
+
+
 """
     FullJacobianFunc(f)
 
 Construct of the Jacobian with ForwardDiff.
 
-When called at point `θ`, the Jacobian matrix is fully instantiated
+When called at point `x`, the Jacobian matrix is fully instantiated
 and stored explicitly in memory.
 
 # Examples
 ```julia
-# forward_model: θ -> Distribution
+# forward_model: x -> Distribution
 jacobian_func = FullJacobianFunc(forward_model)
-jacobian = jacobian_func(θ)  # LinearMap
-jacobian * v  # act on vector
+jacobian = jacobian_func(x)  # LinearMap
+jacobian * δ  # act on vector
 ```
 """
 struct FullJacobianFunc{F<:Function} <: AbstractJacobianFunc
     f::F
 end
 
-function (jf::FullJacobianFunc)(θ::AbstractVector{T}) where T
-    λs = jf.f
-    jac = ForwardDiff.jacobian(λs, θ)
-    LinearMap{T}(jac, isposdef=false, issymmetric=false, ishermitian=false)
+export FullJacobianFunc
+
+function (jf::FullJacobianFunc)(x::AbstractVector{<:Real})
+    f = jf.f
+    y = f(x)
+    U = promote_type(eltype(x), eltype(y))
+    # ForwardDiff.jacobian is not type stable, so convert and fix type:
+    jac = convert(Matrix{U}, ForwardDiff.jacobian(f, x))::Matrix{U}
+    LinearMap{U}(jac, isposdef=false, issymmetric=false, ishermitian=false)
 end
+
+
+
+struct ForwardDiffJVP{F<:Function,T<:AbstractVector{<:Real}}
+    f::F
+    x::T
+end
+
+function (jvp::ForwardDiffJVP)(δ::AbstractVector{<:Real})
+    ForwardDiff.partials.(_dual_along(jvp.f, jvp.x, δ), 1)
+end
+
+
+
+struct ForwardDiffVJP{F<:Function,T<:AbstractVector{<:Real}}
+    f::F
+    x::T
+end
+
+function (vjp::ForwardDiffVJP)(δ::AbstractVector{<:Real})
+    f, x = vjp.f, vjp.x
+
+    # Mathematically elegant, but not type stable and uses double differentiation:
+    #jvp = ForwardDiffJVP(f, x)
+    #g(t) = dot(δ, jvp(t))
+    #ForwardDiff.gradient(g, zero(vjp.x))
+
+    jvp = ForwardDiffJVP(f, x)
+    U = promote_type(eltype(f(x)), eltype(x))
+    result = similar(x, U)
+    tmp = similar(x, U)
+    for i in eachindex(x)
+        tmp .= 0
+        tmp[i] = 1
+        result[i] = dot(δ, jvp(tmp))
+    end
+    result
+end
+
+
+
+struct ZygoteVJP{F<:Function,T,G<:Function}
+    f::F
+    x::T
+    pullback::G
+end
+
+ZygoteVJP(f::Function, x) = ZygoteVJP(f, x, Zygote.pullback(f, x)[2])
+
+@inline (vjp::ZygoteVJP)(δ) = first(vjp.pullback(δ))
+
+
 
 """
     FwdRevADJacobianFunc(f)
@@ -53,24 +112,27 @@ The Jacobian action is computed on the fly, no matrix is stored in memory at any
 
 # Examples
 ```julia
-# forward_model: θ -> Distribution
+# forward_model: x -> Distribution
 jacobian_func = FwdRevADJacobianFunc(forward_model)
-jacobian = jacobian_func(θ)  # LinearMap
-jacobian * v  # act on vector as if it was a matrix
+jacobian = jacobian_func(x)  # LinearMap
+jacobian * δ  # act on vector as if it was a matrix
 ```
 """
 struct FwdRevADJacobianFunc{F<:Function} <: AbstractJacobianFunc
     f::F
 end
 
-function (jf::FwdRevADJacobianFunc)(θ::AbstractVector{T}) where T
-    λs = jf.f
-    num_λs = size(λs(θ), 1)
-    grad_along(f, x, δ) = ForwardDiff.partials.(_dual_along(f, x, δ), 1)
-    jvd(δ) = grad_along(λs, θ, δ)
-    vjd(δ) = first(Zygote.pullback(λs, θ)[2](δ))
-    LinearMap{T}(jvd, vjd, num_λs, size(θ, 1), isposdef=false, issymmetric=false, ishermitian=false)
+export FwdRevADJacobianFunc
+
+function (jf::FwdRevADJacobianFunc)(x::AbstractVector{T}) where T
+    f = jf.f
+    y = f(x)
+    jvp = ForwardDiffJVP(f, x)
+    vjp = ZygoteVJP(f, x)
+    LinearMap{T}(jvp, vjp, size(y, 1), size(x, 1), isposdef=false, issymmetric=false, ishermitian=false)
 end
+
+
 
 """
     FwdDerJacobianFunc(f)
@@ -89,16 +151,12 @@ struct FwdDerJacobianFunc{F<:Function} <: AbstractJacobianFunc
     f::F
 end
 
-function (jf::FwdDerJacobianFunc)(θ::AbstractVector{T}) where T
-    λs = jf.f
-    num_θs = size(θ, 1)
-    num_λs = size(λs(θ), 1)
-    grad_along(f, x, δ) = ForwardDiff.partials.(_dual_along(f, x, δ), 1)
-    jvd(δ) = grad_along(λs, θ, δ)
-    vjd(δ) = ForwardDiff.gradient(t -> dot(δ, jvd(t)), zeros(num_θs))
-    LinearMap{T}(jvd, vjd, num_λs, num_θs, isposdef=false, issymmetric=false, ishermitian=false)
-end
+export FwdDerJacobianFunc
 
-export FullJacobianFunc,
-       FwdRevADJacobianFunc,
-       FwdDerJacobianFunc
+function (jf::FwdDerJacobianFunc)(x::AbstractVector{T}) where T
+    f = jf.f
+    y = f(x)
+    jvp = ForwardDiffJVP(f, x)
+    vjp = ForwardDiffVJP(f, x)
+    LinearMap{T}(jvp, vjp, size(y, 1), size(x, 1), isposdef=false, issymmetric=false, ishermitian=false)
+end
--- test/test_jacobians.jl
+++ test/test_jacobians.jl
@@ -1,10 +1,12 @@
 # This file is a part of MGVI.jl, licensed under the MIT License (MIT).
 
+using Test
 using MGVI
 
 using Distributions
 using LinearAlgebra
 using Random
+using ForwardDiff
 
 if :ModelPolyfit ∉ names(Main)
     include("test_models/model_polyfit.jl")
@@ -12,7 +14,22 @@ if :ModelPolyfit ∉ names(Main)
 end
 
 Test.@testset "test_jacobians_consistency" begin
+    let
+        A = rand(4,6)
+        f = let A=A; x -> (A*(x.^2).^2) end
+        x = rand(6); l = rand(4); r = rand(6)
+        J_ref = ForwardDiff.jacobian(f, x)
+        
+        J1 = @inferred FullJacobianFunc(f)(x)
+        J2 = @inferred FwdRevADJacobianFunc(f)(x)
+        J3 = @inferred FwdDerJacobianFunc(f)(x)
 
+        for J in (J1, J2, J3)
+            @test @inferred(Array(J)) ≈ J_ref
+            @test @inferred(J' * l) ≈ J_ref' * l
+            @test @inferred(J * r) ≈ J_ref * r
+        end
+    end
 
     epsilon = 1E-5
     Random.seed!(145)
